f2c0eaeb308c382728b2ea6082eabddef2cd3caf
jemimajancy <jemimakk2015@gamil.com>
2017-11-11 19:44:02 +0100

    this is jemima

---
+++ odd.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+def main():
+ 	 n= raw_input("enter the no")      
+	 x=int(n)
+         print x
+
+         if x%2==0:
+	 	print("this is even no")
+	 else:
+		print("this odd no")
+
+if __name__=="__main__":
+	    main()
